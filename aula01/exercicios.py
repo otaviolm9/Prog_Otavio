309315271f8afc4951909460f09d835e46a9b48d
Otávio Lemos Morais <otaviolmorais1711@gmail.com>
2026-09-04 15:25:17 +0000

    aula01

--- aula01/exercicios.py
+++ aula01/exercicios.py
@@ -6,30 +6,52 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def soma_lista(lista):
-    """Devolve a soma de todos os numeros da lista. Lista vazia devolve 0."""
-    pass
+    soma = 0
+    for i in range(len(lista)):
+        soma = soma + lista[i]
+    return soma
 
 
 def conta_pares(lista):
-    """Devolve quantos numeros da lista sao pares."""
-    pass
+    contador = 0
+    for num in lista:
+        if num % 2 == 0:
+            contador += 1
+    return contador
 
 
 def maior_valor(lista):
-    """Devolve o maior numero da lista. A lista nao esta vazia."""
-    pass
+    maior = lista[0]
+    for num in lista:
+        if num > maior:
+            maior = num
+    return maior
 
 
 def existe(lista, alvo):
-    """Devolve True se o alvo esta na lista, False se nao esta."""
-    pass
+    for item in lista:
+        if item == alvo:
+            return True
+    return False
 
 
 def busca_linear(lista, alvo):
-    """Devolve a posicao do alvo na lista, ou -1 se ele nao estiver."""
-    pass
+    for i in range(len(lista)):
+        if lista[i] == alvo:
+            return i
+    return -1
 
 
 def segundo_maior(lista):
-    """(Desafio) Devolve o segundo maior, percorrendo a lista uma unica vez."""
-    pass
+    maior = lista[0]
+    segundo = lista[1]
+    if segundo > maior:
+        maior, segundo = segundo, maior
+    for i in range(2, len(lista)):
+        num = lista[i]
+        if num > maior:
+            segundo = maior
+            maior = num
+        elif num > segundo:
+            segundo = num
+    return segundo
