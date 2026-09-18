0106bf4bb3a789b0bdfb270b0a9356cb6f296aea
Otávio Lemos Morais <otaviolmorais1711@gmail.com>
2026-09-18 12:39:59 +0000

    aula02 incompleto

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -6,25 +6,42 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def remove_negativos(lista):
-    """Devolve uma lista nova so com os numeros que nao sao negativos."""
-    pass
+    nova_lista = []
+    contador = 0
 
+    while contador < len(lista): 
+        if lista[contador] >= 0:
+            nova_lista.append(lista[contador])  
+        contador += 1 
+        
+    return nova_lista
 
 def inverte(lista):
-    """Devolve uma lista nova na ordem contraria.
-    Sem usar reverse() e sem usar [::-1]."""
-    pass
+    nova_lista = []
+    for i in range(len(lista) - 1, -1, -1):
+        nova_lista.append(lista[i])
+    return nova_lista
 
 
 def busca_binaria(lista, alvo):
-    """Recebe uma lista JA ORDENADA. Devolve a posicao do alvo, ou -1."""
-    pass
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        
+        if lista[meio] == alvo:
+            return meio
+        elif lista[meio] < alvo:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+    return -1
 
 
 def intercala(lista_a, lista_b):
-    """Devolve uma lista nova alternando os elementos das duas.
-    As duas listas tem o mesmo tamanho."""
-    pass
+    return [item for par in zip(lista_a, lista_b) for item in par]
+
 
 
 def remove_repetidos(lista):
